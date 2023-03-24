e4b5ebb0c1dfad6732c4870fd0e9a0af5061fcb9
Sasha <aapreutes@gmail.com>
2023-03-25 00:04:22 +0200

    Add gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+
+const galleryItemsList = document.querySelector(".gallery");
+const gallery = createPicGallery(galleryItems);
+
+galleryItemsList.insertAdjacentHTML("beforeend", gallery);
+
+function createPicGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+    `;
+    })
+    .join("");
+}
 
 console.log(galleryItems);
